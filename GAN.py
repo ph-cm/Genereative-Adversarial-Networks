75adcff045cbaa911b9cc6356cc5a3ad074df1c7
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2024-12-14 13:34:44 -0300

    Deep Convolutional GAN

--- GAN.py
+++ GAN.py
@@ -159,3 +159,140 @@ models = (generator, discriminator)
 optimizers = (optimizer_generator, optimizer_discriminator)
 
 train_gan(dataloaders, models, optimizers, loss_fn, epochs, plot_every, device)
+
+#DCGAN
+class DCGenerator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.ConvTranspose2d(100, 256, kernel_size=(3, 3), stride=(2, 2), bias=False)
+        self.bn1 = nn.BatchNorm2d(256)
+        self.conv2 = nn.ConvTranspose2d(256, 128, kernel_size=(3, 3), stride=(2, 2), bias=False)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.conv3 = nn.ConvTranspose2d(128, 64, kernel_size=(3, 3), stride=(2, 2), bias=False)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.conv4 = nn.ConvTranspose2d(64, 1, kernel_size=(3, 3), stride=(2, 2), padding=(2, 2), output_padding=(1, 1), bias=False)
+        self.tanh = nn.Tanh()
+        self.relu = nn.ReLU()
+    
+    def forward(self, input):
+        hidden1 = self.relu(self.bn1(self.conv1(input)))
+        hidden2 = self.relu(self.bn2(self.conv2(hidden1)))
+        hidden3 = self.relu(self.bn3(self.conv3(hidden2)))
+        generated =  self.tanh(self.conv4(hidden3)).view(input.shape[0], 1, 28, 28)
+        return generated
+    
+class DCDiscriminator(nn.Module):
+    def __init__(self):
+        super().__init__()
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False)
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False)
+        self.bn2 = nn.BatchNorm2d(128)
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False)
+        self.bn3 = nn.BatchNorm2d(256)
+        self.conv4 = nn.Conv2d(256, 1, kernel_size=(4, 4), stride=(2, 2), padding=(1, 1), bias=False)
+        self.leaky_relu = nn.LeakyReLU(0.2)
+        self.sigmoid = nn.Sigmoid()
+    
+    def forward(self, input):
+        hidden1 = self.leaky_relu(self.conv1(input))
+        hidden2 = self.leaky_relu(self.bn2(self.conv2(hidden1)))
+        hidden3 = self.leaky_relu(self.bn3(self.conv3(hidden2)))
+        classified = self.sigmoid(self.conv4(hidden3)).view(input.shape[0], -1)
+        return classified
+def weights_init(model):
+    classname = model.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(model.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(model.weight.data, 1.0, 0.02)
+        nn.init.constant_(model.bias.data, 0)
+transform = transforms.Compose([
+    transforms.ToTensor(),
+])
+
+train_dataset, test_dataset = mnist(train_size, transform)
+train_dataloader = torch.utils.data.DataLoader(train_dataset, drop_last=True, batch_size=batch_size, shuffle=True)
+dataloaders = (train_dataloader, )
+
+generator = DCGenerator().to(device)
+generator.apply(weights_init)
+discriminator = DCDiscriminator().to(device)
+optimizer_generator = optim.Adam(generator.parameters(), lr=lr, weight_decay=weight_decay, betas=(beta1,beta2))
+optimizer_discriminator = optim.Adam(discriminator.parameters(), lr=lr, weight_decay=weight_decay, betas=(beta1, beta2))
+loss_fn = nn.BCELoss()
+
+models = (generator, discriminator)
+optimizers = (optimizer_generator, optimizer_discriminator)
+
+def dcplotn(n, generator, device):
+    generator.eval()
+    noise = torch.FloatTensor(np.random.normal(0, 1, (n, 100, 1, 1))).to(device)
+    imgs = generator(noise).detach().cpu()
+    fig, ax = plt.subplots(1, n)
+    for i, im in enumerate(imgs):
+        ax[i].imshow(im[0])
+    plt.show()
+
+def train_dcgan(dataloaders, models, optimizers, loss_fn, eposchs, plot_every, device):
+    tqdm_iter = tqdm(range(epochs))
+    train_dataloader = dataloaders[0]
+    
+    gen, disc = models[0], models[1]
+    optim_gen, optim_disc = optimizers[0], optimizers[1]
+    
+    gen.train()
+    disc.train()
+    
+    for epoch in tqdm_iter:
+        train_gen_loss = 0.0
+        train_disc_loss = 0.0
+        
+        test_gen_loss = 0.0
+        test_disc_loss = 0.0
+        
+        for batch in train_dataloader:
+            imgs, _ = batch
+            imgs = imgs.to(device)
+            imgs = 2.0 * imgs - 1.0
+            
+            gen.zero_grad()
+            
+            noise = torch.FloatTensor(np.random.normal(0.0, 1.0, (imgs.shape[0], 100, 1, 1))).to(device)
+            real_labels = torch.ones((imgs.shape[0], 1)).to(device)
+            fake_labels = torch.zeros((imgs.shape[0], 1)).to(device)
+            
+            generated = gen(noise)
+            disc_preds = disc(generated)
+            
+            g_loss = loss_fn(disc_preds, real_labels)
+            g_loss.backward()
+            optim_gen.step()
+            
+            disc.zero_grad()
+            
+            disc_real = disc(imgs)
+            disc_real_loss = loss_fn(disc_real, real_labels)
+            
+            disc_fake = disc(generated.detach())
+            disc_fake_loss = loss_fn(disc_fake, fake_labels)
+            
+            d_loss = (disc_real_loss + disc_fake_loss) / 2.0
+            d_loss.backward()
+            optim_disc.step()
+            
+            train_gen_loss += g_loss.item()
+            train_disc_loss += d_loss.item()
+            
+        train_gen_loss /= len(train_dataloader)
+        train_disc_loss /= len(train_dataloader)
+        
+        if epoch % plot_every == 0 or epoch == epochs - 1:
+            dcplotn(5, gen, device)
+        
+        tqdm_dct = {'generator loss: ': train_gen_loss, 'discriminator loss: ': train_disc_loss}
+        tqdm_iter.set_postfix(tqdm_dct, refresh=True)
+        tqdm_iter.refresh()
+        
+train_dcgan(dataloaders, models, optimizers, loss_fn, epochs // 2, plot_every // 2, device)
+
+            
