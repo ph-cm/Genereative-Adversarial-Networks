46371d9dccd5166a559869b6edda416fb2bc55da
Pedro Henrique <trabalho.prog.pedro@gmail.com>
2024-12-16 08:28:18 -0300

    Implementing Generator

--- DCGAN.py
+++ DCGAN.py
@@ -41,6 +41,9 @@ nc = 3
 #Size of z latent vector (size of generator input)
 nz = 100
 
+#Size of feature maps in generator
+ngf = 64
+
 #Size of feature maps in discriminator 
 ndf = 64
 
@@ -80,3 +83,66 @@ plt.axis("off")
 plt.title("Training Images")
 plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
 plt.show()
+
+#Implementation
+
+#Weight Initialization
+#custom weights initialization called on 'netG' and 'netD'
+def weights_init(m):
+    classname = m.__class__.__name__
+    if classname.find('Conv') != -1:
+        nn.init.normal_(m.weight.data, 0.0, 0.02)
+    elif classname.find('BatchNorm') != -1:
+        nn.init.normal_(m.weight.data, 1.0, 0.02)
+        nn.init.constant_(m.bias.data, 0)
+
+#generator
+class Generator(nn.Module):
+    def __init__(self, ngpu):
+        super(Generator, self).__init__()
+        self.ngpu = ngpu
+        self.main = nn.Sequential(
+            #input is Z, going into a convolution
+            nn.ConvTranspose2d(nz, ngf * 8, 4, 1, 0, bias=False),
+            nn.BatchNorm2d(ngf * 8),
+            nn.ReLU(True),
+
+            #state size  4 x 4
+            nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 4),
+            nn.ReLU(True),
+            
+            #state size 8 x 8
+            nn.ConvTranspose2d(ngf * 4, ngf * 2, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf * 2),
+            nn.ReLU(True),
+            
+            #state size 16 x 16
+            nn.ConvTranspose2d(ngf * 2, ngf, 4, 2, 1, bias=False),
+            nn.BatchNorm2d(ngf),
+            nn.ReLU(True),
+            
+            #state size 32 x 32
+            nn.ConvTranspose2d(ngf, nc, 4, 2, 1, bias=False),
+            nn.Tanh()
+            
+            #state zise 64 x 64
+        )
+    def forward(self, input):
+        return self.main(input)
+    
+#Create the generator 
+netG = Generator(ngpu).to(device)
+
+#Handle multi-GPU if desired
+if (device.type == 'cuda') and (ngpu > 1):
+    netG = nn.DataParallel(netG, list(range(ngpu)))
+
+#Apply the 'weights_init' function to randomly initialize all weights
+# to 'mean=0', 'stdev=0.02'
+netG.apply(weights_init)
+
+#Print Model
+print(netG)
+    
+    
